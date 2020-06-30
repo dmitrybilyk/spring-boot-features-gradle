1ce4b33766a6deb4c9fada349932037aa3dcd892
dmitry <dmitry.b@scopicsoftware.com>
2020-06-30 19:26:00 +0300

    Initial commit

---
+++ src/main/java/com/spring/featuresgradle/FeaturesGradleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.featuresgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturesGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaturesGradleApplication.class, args);
+	}
+
+}
